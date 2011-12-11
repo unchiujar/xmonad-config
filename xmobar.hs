bf49917532bd56c7a5d684229c604b968f1cabff
Vasile Jureschi <vasile.jureschi@gmail.com>
2011-12-11 02:22:26 +0200

    added disk I/O info, changed to use statistics from i3status, changed to Cluj weather station and centigrade, changed for 1920 monitor and 1280 laptop size

--- xmobar.hs
+++ xmobar.hs
@@ -1,25 +1,29 @@
 -- xmobar config used by Vic Fryzel
 -- Author: Vic Fryzel
 -- http://github.com/vicfryzel/xmonad-config
-
+-- modified by Vasile Jureschi 
+-- https://github.com/unchiujar/xmonad-config
+-- weather station codes http://weather.rap.ucar.edu/surface/stations.txt
 -- This is setup for dual 1680x1050 monitors, with the right monitor as primary
 Config {
     font = "xft:Fixed-8",
     bgColor = "#000000",
     fgColor = "#ffffff",
-    position = Static { xpos = 1680, ypos = 0, width = 1560, height = 16 },
+    position = Static { xpos = 1920, ypos = 0, width = 1000, height = 16 },
     lowerOnStart = True,
     commands = [
-        Run Weather "KNYC" ["-t","<tempF>F <skyCondition>","-L","64","-H","77","--normal","#CEFFAC","--high","#FFB6B0","--low","#96CBFE"] 36000,
+        Run Weather "LRCL" ["-t","<tempC>°C <skyCondition>","-L","64","-H","77","--normal","#CEFFAC","--high","#FFB6B0","--low","#96CBFE"] 36000,
         Run Cpu ["-L","3","-H","50","--normal","#CEFFAC","--high","#FFB6B0"] 10,
-        Run MultiCpu ["-t","Cpu: <total0><total1><total2><total3>","-L","3","-H","50","--normal","#CEFFAC","--high","#FFB6B0","-w","5"] 10,
+        -- %multicpu% Run MultiCpu ["-t","Cpu: <total0><total1>","-L","3","-H","50","--normal","#CEFFAC","--high","#FFB6B0","-w","5"] 10,
         Run Memory ["-t","Mem: <usedratio>%"] 10,
         Run Swap [] 10,
-        Run Network "eth0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 5,
+        Run Network "usb0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 5,
         Run Date "%a %b %_d %l:%M" "date" 10,
+        Run PipeReader "/tmp/i3.fifo" "i3",
+        Run PipeReader "/tmp/diskio.fifo" "diskio",
         Run StdinReader
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader% }{ %multicpu%   %memory%   %swap%   %eth0%   <fc=#FFFFCC>%date%</fc>   %KNYC%"
+    template = "%StdinReader% }{ %i3% | %cpu% | %memory% | %swap% | %usb0% | <fc=#FFFFCC>%date%</fc>| %LRCL%"
 }
